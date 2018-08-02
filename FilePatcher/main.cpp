81902a74867d93d2f8c4d48d67a2b029e92b3628
Exp <theexp111@gmail.com>
2018-08-02 14:46:42 +0200

    Did PatternFinder (stolen from alliedmods)

--- FilePatcher/main.cpp
+++ FilePatcher/main.cpp
@@ -8,32 +8,39 @@ using namespace std;
 #define getBits(x) (INRANGE((x & (~0x20)), 'A', 'F') ? ((x & (~0x20)) - 'A' + 0xA): (INRANGE(x, '0', '9') ? x - '0': 0))
 #define getByte(x) (getBits(x[0]) << 4 | getBits(x[1]))
 
-DWORD FindPattern(unsigned char* buffer, DWORD fileSize, const char* targetPattern)
+DWORD FindPattern(unsigned char* buffer, DWORD fileSize, string targetPattern)
 {
-	const char* pattern = targetPattern;
-
-	uintptr_t firstMatch = 0;
-
-	for (size_t position = 0; position < fileSize; position++) {
-		if (!*pattern)
-			return firstMatch;
-		
-		const uint8_t patternCurrent = *reinterpret_cast<const uint8_t*>(pattern);
-		const uint8_t memoryCurrent = *reinterpret_cast<const uint8_t*>(buffer[position]);
-
-		if (patternCurrent == '\?' || memoryCurrent == getByte(pattern)) {
-			if (!firstMatch)
-				firstMatch = position;
-
-			if (!pattern[2])
-				return firstMatch;
-
-			pattern += patternCurrent != '\?' ? 3 : 2;
+	string newPattern = "";
+	for (size_t i = 0; i < targetPattern.size(); i++) //Remove spaces and convert bytes to char so we can compare with buffer char
+	{
+		if (targetPattern[i] == ' ')
+			continue;
+		if (targetPattern[i] == '?')
+		{
+			newPattern += '?';
+			continue;
 		}
-		else {
-			pattern = targetPattern;
-			firstMatch = 0;
+
+		newPattern += (getBits(targetPattern[i]) << 4 | getBits(targetPattern[i + 1]));
+		i++;
+	}
+
+	size_t sigSize = newPattern.size();
+	unsigned char *pBasePtr = buffer;
+	unsigned char *pEndPtr = buffer + fileSize;
+	size_t i;
+
+	while (pBasePtr < pEndPtr) {
+		for (i = 0; i < sigSize; i++) {
+			if ((newPattern[i] != '?') && (newPattern[i] != pBasePtr[i]))
+				break;
 		}
+
+		// If 'i' reached the end, we know we have a match!
+		if (i == sigSize)
+			return pBasePtr - buffer;
+
+		pBasePtr++;
 	}
 
 	return NULL;
@@ -52,7 +59,7 @@ int main()
 {
 	int type = 0;
 	DWORD offset = 0;
-	string signaturePattern = "";
+	string signaturePattern;
 	string fileName = "";
 	HANDLE hFile;
 	BYTE* patchBytes;
@@ -67,7 +74,7 @@ int main()
 	// file handle
 	hFile = INVALID_HANDLE_VALUE;
 	// open input file in write mode (use GENERIC_WRITE flag)
-	hFile = CreateFile(fileName.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	hFile = CreateFile(fileName.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
@@ -100,6 +107,7 @@ int main()
 		cin >> hex >> offset;
 		break;
 	case 2:
+		cin.ignore();
 		getline(cin, signaturePattern);
 		break;
 	default:
@@ -108,7 +116,7 @@ int main()
 
 	if (type == 2) //Check the signature -> get offset
 	{
-		offset = FindPattern(hFile, signaturePattern.c_str());
+		offset = FindPattern(buffer, fileSize, signaturePattern);
 
 		if (offset == 0)
 		{
@@ -116,6 +124,11 @@ int main()
 			system("PAUSE");
 			return -1;
 		}
+
+		cout << "Offset the address by how much:" << endl;
+		int offoffset = 0;
+		cin >> offoffset;
+		offset += offoffset;
 	}
 
 	cout << "Type in how many bytes you want to patch:" << endl;
@@ -142,7 +155,7 @@ int main()
 
 	SetFilePointer(hFile, offset, 0, FILE_BEGIN);
 
-	bool result = WriteFile(hFile, patchBytes, sizeof(patchBytes), &written, NULL);
+	bool result = WriteFile(hFile, patchBytes, size, &written, NULL);
 
 	cout << "Result: " << result << ". Bytes written: " << written << endl;
 
